158bf11192a35b558a497c58e86ab448d69b8a67
Arseni Edel <edel.ars@gmail.com>
2024-03-12 15:38:10 -0300

    some fix debug esc seq mode

--- pkg/escape_sequence_parser/debug.go
+++ pkg/escape_sequence_parser/debug.go
@@ -12,31 +12,31 @@ type esDebug struct {
 	file *os.File
 }
 
-func (esdebug *esDebug) ParseEscapeSequence(buf []byte) []EscapeSequenceResultIface {
-	res := esdebug.next.ParseEscapeSequence(buf)
+func (e *esDebug) ParseEscapeSequence(buf []byte) []EscapeSequenceResultIface {
+	res := e.next.ParseEscapeSequence(buf)
 
-	if esdebug.file != nil && len(buf) > 1 && buf[0] == 0x1b {
-		esdebug.file.WriteString(fmt.Sprintf("data: %v\n", hex.EncodeToString(buf)))
-		esdebug.file.WriteString(fmt.Sprintf("count actions: %d \n", len(res)))
+	if e.file != nil && len(buf) > 1 && buf[0] == 0x1b {
+		e.file.WriteString(fmt.Sprintf("! block begin, raw binary data: %v\n", hex.EncodeToString(buf)))
+		e.file.WriteString(fmt.Sprintf("count actions: %d \n", len(res)))
 		for c, v := range res {
 			a := string(rune(v.GetAction()))
 			if v.GetAction() == 0x00 {
 				a = "none"
 			}
-			esdebug.file.WriteString(fmt.Sprintf("N %d action type: %s \n", c+1, a))
-			// fmt.Sprintf("N %d action type: %s \n", c+1, strconv.FormatInt(int64(v.GetAction()), 16)),
-
-			esdebug.file.WriteString(fmt.Sprintf("N %d raw args: %s \n", c+1, string(v.GetRaw())))
+			e.file.WriteString(fmt.Sprintf("#%d action type: %s \n", c+1, a))
+			e.file.WriteString(fmt.Sprintf("#%d raw args: %s \n", c+1, string(v.GetRaw())))
 			n1, n2 := v.GetIntsFromArgs()
-			esdebug.file.WriteString(fmt.Sprintf("N %d int args: %d, %d \n", c+1, n1, n2))
-			esdebug.file.WriteString(fmt.Sprintf("\n"))
+			e.file.WriteString(fmt.Sprintf("#%d int args: %d, %d \n\n", c+1, n1, n2))
 		}
+		e.file.WriteString(fmt.Sprintf("! end block\n\n"))
 	}
 	return res
 }
 
-func (esdebug *esDebug) Stop() {
-	esdebug.file.Close()
+func (e *esDebug) Stop() {
+	if e.file != nil {
+		e.file.Close()
+	}
 }
 
 func NewESDebug(next EscapeSequenceParserIface, debugOpts configuration.Debug) esDebug {
@@ -44,7 +44,7 @@ func NewESDebug(next EscapeSequenceParserIface, debugOpts configuration.Debug) e
 	if debugOpts.EscapeSequence && debugOpts.DebugLogFile != "" {
 		file, err := os.Create(debugOpts.DebugLogFile)
 		if err != nil {
-			fmt.Println("Unable to create file:", err)
+			fmt.Printf("Unable to create debug log file:%s %s\n", debugOpts.DebugLogFile, err)
 			os.Exit(1)
 		}
 		r.file = file
